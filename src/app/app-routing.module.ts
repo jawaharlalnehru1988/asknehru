020fc27e1b4e8e1663d14bedc843ca9d945592dc
Jawaharlal Nehru E <Jawaharlal@wolkensoftware.com>
2023-10-01 22:47:01 +0530

    github module is removed newly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,17 +13,11 @@ const routes: Routes = [
   {path: 'bootstrap', loadChildren: ()=> import('./bootstrap/bootstrap.module').then(m=> m.BootstrapModule)},
   {path: 'typescript', loadChildren: ()=> import('./typescript/typescript.module').then(m => m.TypescriptModule)},
   {path: 'materialAngular', loadChildren: ()=> import('./material/material.module').then(m => m.MaterialModule)},
-  {path: 'nodejs', loadChildren: ()=> import('./nodejs/nodejs.module').then(m => m.NodejsModule)},
   {path: 'fxlayout', loadChildren: ()=> import('./fxlayout/fxlayout.module').then(m => m.FxlayoutModule)},
   {path: 'aggrid', loadChildren: ()=> import('./aggrid/aggrid.module').then(m=>m.AggridModule)},
   {path: 'rxjs', loadChildren: ()=> import('./rxjs/rxjs.module').then(m => m.RxjsModule)},
-  {path: 'restapi', loadChildren: ()=>import('./restapi/restapi/restapi.module').then(m => m.RestapiModule)},
   {path: 'git', loadChildren: ()=>import('./git/git/git.module').then(m => m.GitModule)},
-  {path: 'bitbucket', loadChildren: ()=> import('./bitbucket/bitbucket/bitbucket.module').then(m => m.BitbucketModule)},
-  {path: 'python', loadChildren: ()=> import('./python/python/python.module').then(m => m.PythonModule)},
   {path: 'karmajasmine', loadChildren: ()=> import('./karmajasmin/karmajasmin/karmajasmin.module').then(m => m.KarmajasminModule)},
-  {path: 'express', loadChildren: ()=> import('./expressjs/expressjs/expressjs.module').then(m=> m.ExpressjsModule)},
-  {path: 'mongo', loadChildren: ()=> import('./mongodb/mongodb/mongodb.module').then(m => m.MongodbModule)}
 ];
 
 @NgModule({
