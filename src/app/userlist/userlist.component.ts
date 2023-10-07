f58bf90e7c8f77b1d67aad6d5e5fe8d83ea8fdf9
Jawaharlal Nehru E <Jawaharlal@wolkensoftware.com>
2023-10-07 18:18:52 +0530

    delete request done successfully

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from '../api.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  fruit: string;
+}
 @Component({
   selector: 'app-userlist',
   templateUrl: './userlist.component.html',
-  styleUrls: ['./userlist.component.scss']
+  styleUrls: ['./userlist.component.scss'],
 })
 export class UserlistComponent implements OnInit {
   usersData!: any[];
+  displayedColumns: string[] = ['id', 'name', 'gender', 'email', 'password', 'role', 'action'];
+  dataSource!: MatTableDataSource<UserData>;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   constructor(private userService: ApiService) {}
 
   ngOnInit(): void {
@@ -16,7 +30,29 @@ export class UserlistComponent implements OnInit {
       this.usersData = data;
       console.log('this.usersData :', this.usersData);
       // Perform any component-specific logic with usersData
+      this.dataSource = new MatTableDataSource(this.usersData);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
   }
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
 
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  deleteRow(idToDelete: any){
+    this.userService.deleteRecordById(idToDelete).subscribe({
+      next:((res:any)=>{
+        alert(`Record with id number " ${idToDelete} " deleted successfully`);
+      }), error:((err:any)=>{
+        alert("Some server error is there while deleting your data");
+      })
+    }
+    )
+
+  }
 }
