8019a3dd9ec5182b5d0ce545289cc8eceab15cb3
nehru00123@gmail.com <nehru00123@gmail.com>
2023-11-18 15:39:21 +0530

    podcasts are filled with files

--- src/app/angular/angular/angular.component.ts
+++ src/app/angular/angular/angular.component.ts
@@ -13,37 +13,13 @@ hljs.registerLanguage('html', html);
   templateUrl: './angular.component.html',
   styleUrls: ['./angular.component.scss']
 })
-export class AngularComponent implements OnInit, AfterViewInit {
-  isLinear = false;
-  agIntro: any;
-  topics: any;
+export class AngularComponent implements OnInit {
+ 
 
   constructor(private agServie: AngularService, private router: Router) { }
 
   ngOnInit(): void {
-    this.agServie.getAgcourse().subscribe({
-      next: (res)=>{
-        res.courses.map((item:any)=>{
-          if (item.id === 7) {
-            this.agIntro = item;
-            this.topics = item.topics;
-            console.log(this.topics);
-            
-          }
-        });      
-      }
-
-    })
-    }  
-    ngAfterViewInit() {
-      const codeBlocks = Array.from(document.querySelectorAll('pre code')) as HTMLElement[];
-      codeBlocks.forEach((codeBlock) => {
-        hljs.highlightBlock(codeBlock);
-      });
-    }
-  
     
-    printTopicId(topicId: number) {
-      this.router.navigate(['agdetail']);
-    }
+    } 
+   
 }
