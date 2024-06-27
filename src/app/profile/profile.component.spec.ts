ac0c63324d45289d82d508305e7ef990c96d72ba
nehru00123@gmail.com <nehru00123@gmail.com>
2024-06-27 09:05:02 +0530

    profile design changes

--- src/app/profile/profile.component.spec.ts
+++ src/app/profile/profile.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProfileComponent } from './profile.component';
+import { Person, ProfileComponent } from './profile.component';
 
-describe('ProfileComponent', () => {
+fdescribe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
 
@@ -20,4 +20,27 @@ describe('ProfileComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should return average age", ()=>{
+    const people = [
+      {name: "ram", age: 23},
+      {name: "keshav", age: 27},
+    ];
+
+    const result = component.calculateAverageAge(people);
+    expect(result).toBe(25);
+  });
+
+  it('should return zero if the array is empty', ()=>{
+    const people:Person[] = [];
+    const result = component.calculateAverageAge(people);
+    expect(result).toBe(0);    
+  });
+
+  it('should return single person correctly', ()=>{
+    const person: Person[] = [{name: "ram", age: 24}];
+    const result = component.calculateAverageAge(person);
+    expect(result).toBe(24); 
+  })
+
 });
