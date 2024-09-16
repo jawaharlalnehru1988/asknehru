a23e67703499518aec5dd68d7970e916cdcc7f69
nehru00123@gmail.com <nehru00123@gmail.com>
2024-09-17 02:40:44 +0530

    angular demystified component is added for articles

---
+++ src/app/angular-demystify/angular-demystify.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-demystify',
+  standalone: true,
+  imports: [],
+  templateUrl: './angular-demystify.component.html',
+  styleUrl: './angular-demystify.component.scss'
+})
+export class AngularDemystifyComponent {
+
+}
--- src/core/routes/app.route.ts
+++ src/core/routes/app.route.ts
@@ -8,13 +8,6 @@ export const routes: Routes = [
       import('../../app/home/home.component').then((m) => m.HomeComponent),
   },
   {
-    path: 'register',
-    loadComponent: () =>
-      import('../../app/register/register.component').then(
-        (m) => m.RegisterComponent
-      ),
-  },
-  {
     path: 'typescript',
     loadComponent: () =>
       import('../../app/typescript/typescript.component').then(
