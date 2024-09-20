edc5bcf834b7813b8e5595a67fd1881a22d3e7a9
nehru00123@gmail.com <nehru00123@gmail.com>
2024-09-20 08:41:43 +0530

    javascript added

--- src/app/angular-demystify/rxjs/rxjs.component.ts
+++ src/app/angular-demystify/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { filter, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './rxjs.component.scss'
 })
 export class RxjsComponent {
+observable$ = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
+ngOnInit() {  
+    const modifiedObservable$ = this.observable$.pipe(
+      filter(num => num % 2 === 0),
+      map(num => num * 10)
+    );
+
+    modifiedObservable$.subscribe(x => console.log(x));
+}
 }
