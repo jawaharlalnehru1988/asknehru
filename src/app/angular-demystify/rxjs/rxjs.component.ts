81705abd5da8935056fa9d813a56cd251cd430d2
nehru00123@gmail.com <nehru00123@gmail.com>
2024-09-25 08:55:27 +0530

    youtube is added

--- src/app/angular-demystify/rxjs/rxjs.component.ts
+++ src/app/angular-demystify/rxjs/rxjs.component.ts
@@ -1,32 +1,30 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { combineLatest, interval, fromEvent } from 'rxjs';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { YouTubePlayerModule } from '@angular/youtube-player';
+import { combineLatest, interval, fromEvent, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
   standalone: true,
-  imports: [],
+  imports: [YouTubePlayerModule],
   templateUrl: './rxjs.component.html',
-  styleUrls: ['./rxjs.component.scss']  // Corrected property name
+  styleUrls: ['./rxjs.component.scss'], // Corrected property name
 })
-export class RxjsComponent  {
-
-  @ViewChild('filterInput') filterInput!: ElementRef;
-
+export class RxjsComponent {
+  @ViewChild('clickButton',) button!: ElementRef;
+  @ViewChild('inputText') inputText!: ElementRef;
 
 
   ngAfterViewInit() {
 
-    const keyUp$ = fromEvent<KeyboardEvent>(this.filterInput.nativeElement, 'keyup');
+   const inputText$ = fromEvent<KeyboardEvent>
+   (this.inputText.nativeElement, 'input');
 
-    const filter$ = keyUp$.pipe(
-      map((event: KeyboardEvent)=> (event.target as HTMLInputElement).value)
-    )
-
-
-    filter$.subscribe(value => console.log(value));
-
+   inputText$.pipe(
+    map((event: KeyboardEvent) => 
+      (event.target as HTMLInputElement)?.value)
+   ).subscribe((value:string) => console.log( value))
   }
 
-
+  ngOnDestroy() {}
 }
