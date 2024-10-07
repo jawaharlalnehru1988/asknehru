954d73a91d7a1f1a6fbc11d6c267887f602cbf5d
nehru00123@gmail.com <nehru00123@gmail.com>
2024-10-07 20:55:30 +0530

    rxjs

--- src/app/angular-demystify/rxjs/rxjs.component.ts
+++ src/app/angular-demystify/rxjs/rxjs.component.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component} from '@angular/core';
-import { concat } from 'core-js/core/array';
-import { concatMap, filter, first, from, last, map, merge, mergeMap, Observable, of, skip, switchMap, take, takeLast, takeUntil, tap} from 'rxjs';
+import { concatMap, from,} from 'rxjs';
 import { ApiService } from 'src/app/api.service';
 
 @Component({
@@ -13,33 +11,17 @@ import { ApiService } from 'src/app/api.service';
 })
 export class RxjsComponent {
   users: any[] = [];
-  constructor(private apiService: ApiService, private http: HttpClient) {}
+  constructor(private apiService: ApiService) {}
   ngOnInit() {
-    const userIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const userIds = [1, 3, 4, 6, 8];
 
-    from(userIds)
-      .pipe(concatMap((id) => this.apiService.getDataFromAddress(id)))
-      .subscribe((user) => {
-        console.log('user :', user);
-        this.users.push(user);
-      });
-    console.log(' this.users :', this.users);
-  }
-
-  getUserData() {
-    this.apiService.getAllEssays().pipe(
-      mergeMap((data:any) => {
-        return from(data);
-      }),
-      skip(4),
-    ).subscribe((response: any) => {
-        console.log('response :', response);
-
-      });
-  }
-
-  addUser() {
-    this.getUserData();
+    from(userIds).pipe(
+      concatMap((id) => this.apiService.getDataFromAddress(id))
+    ).subscribe((user)=>{
+      this.users.push(user);
+      console.log('this.users :', this.users);
+    })
+        
   }
 }
 
