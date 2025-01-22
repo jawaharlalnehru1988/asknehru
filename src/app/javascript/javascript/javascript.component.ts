37a03d437bc8a4e2b7cd8d60541a4631bca5a815
nehru00123@gmail.com <nehru00123@gmail.com>
2025-01-22 08:19:35 +0530

    aligned topics

--- src/app/javascript/javascript/javascript.component.ts
+++ src/app/javascript/javascript/javascript.component.ts
@@ -2,20 +2,24 @@ import { Component } from '@angular/core';
 import { JssidebarComponent } from "../../jssidebar/jssidebar.component";
 import { NgClass } from '@angular/common';
 import { JsModel, Jstopics } from './jstopics';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-javascript',
     standalone: true,
-    imports: [JssidebarComponent, NgClass],
+    imports: [JssidebarComponent, MatIconModule],
     templateUrl: './javascript.component.html',
     styleUrl: './javascript.component.scss'
 })
 export class JavascriptComponent {
     isArrowActive = false;
     jstopic: string = "";
+    mainTopicAppear: boolean = false;
 
    jstopics = new Jstopics();
-   topics = this.jstopics.topics;
+   topics = this.jstopics.categorisedTopics;
+    subTopicTitle: any;
+    subTopicArray: any;
 
     toggleArrow(): void {
       this.isArrowActive = !this.isArrowActive;
@@ -26,7 +30,16 @@ export class JavascriptComponent {
 
     }
 
-    clickTopic(topic: JsModel){
-        console.log('topic :', topic);
+    clickTopic(topic: any){
+        this.subTopicTitle = topic.title;
+        this.subTopicArray = topic.topics;     
+        this.mainTopicAppear = true;                                                                                                                                             
+    }
+
+    subTopicClicked(subTopic:any){
+        console.log('subTopic :', subTopic);
+    }
+    backToMainTopic(){
+        this.mainTopicAppear = false;
     }
 }
