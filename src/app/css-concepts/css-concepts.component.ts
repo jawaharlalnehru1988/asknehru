c439e32390451487a593f38b9b584eff9633c5e0
nehru00123@gmail.com <nehru00123@gmail.com>
2025-02-02 13:34:11 +0530

    css page cultivated

--- src/app/css-concepts/css-concepts.component.ts
+++ src/app/css-concepts/css-concepts.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { CssConcept } from './cssConcept';
+import { CssConcept, CssMainContent } from './cssConcept';
 
 @Component({
   selector: 'app-css-concepts',
@@ -10,21 +10,49 @@ import { CssConcept } from './cssConcept';
 })
 export class CssConceptsComponent extends CssConcept {
   
-  cssMainContent: SafeHtml = "";
+  cssMainContent: string[] = this.mainContent[0].list;
+  cssCode: string = `
+  // HTML
+     <div class="simple-box">This is a simple square.</div>
+
+  // CSS
+    .simple-box {
+      width: 100px;
+      height: 100px;
+      background-color: lightblue;
+      padding: 20px;
+      border: 2px solid navy;
+    }
+  `;
+
+  inlineCssShape: string = `<div style="width: 100px; height: 100px; background-color: lightblue; padding: 20px; border: 2px solid navy;">This is a simple square.</div>`;
+
+  projectedCSSShape: SafeHtml = this.sanitizer.bypassSecurityTrustHtml(this.inlineCssShape);
+  cssConcepts: string[] = [];
 
   constructor(private sanitizer: DomSanitizer){
     super();
-    // this.cssMainContent = this.sanitizer.bypassSecurityTrustHtml(this.mainContent[0].content);
+  }
+
+  ngOnInit(): void {
+  this.cssConcepts  = this.mainContent.map(content => content.short);
+
   }
 
   selectSideTopic(title:string){  
     const selected = this.mainContent.find(content => content.short === title);
-    // if(selected){
-    //   this.cssMainContent = this.sanitizer.bypassSecurityTrustHtml(selected.content);
-    // } else {
-    //   this.cssMainContent = this.sanitizer.bypassSecurityTrustHtml(this.mainContent[0].content);
-    // }
+    if(selected){
+      this.cssMainContent = selected.list;
+    } else {
+      this.cssMainContent = this.mainContent[0].list;
+    }
   }
 
+  selectSubTopic(title:string){
+  console.log('title :', title);
+    
+  }
+
+  
 
 }
